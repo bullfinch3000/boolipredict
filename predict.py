3fe4ce7d9e36fec9aa4436edf33eb3deeeeb4441
Henrik Almér <henrik.almer@gmail.com>
2016-02-02 21:26:49 +0100

    Prediction command line program

--- predict.py
+++ predict.py
@@ -1,15 +1,51 @@
+# usage: python predict.py livingArea rooms floor rent areaName
+# example: python predict.py 48 2 2 2800 Abrahamsberg
+
+import sys
+
+from models import *
 import utils
 
-# load SVM fit
-clf = utils.getSVM()
+def main(args=None):
+    # simple args parsing
+    if args is None:
+        args = sys.argv[1:]
+    if len(args) != 5:
+        print "Wrong number of arguments"
+        sys.exit(2)
+    try:
+        livingArea = int(args[0])
+        rooms = int(args[1])
+        floor = int(args[2])
+        rent = int(args[3])
+        areaName = args[4]
+    except:
+        print "Wrong argument type"
+        sys.exit(2)
 
-# load scaler
-scaler = utils.getScaler()
+    # read area price index from db
+    try:
+        area = Area.get(Area.name==areaName)
+    except Area.DoesNotExist:
+        print "Area not found"
+        sys.exit(2)
 
-# predict
-# soldPrice,livingArea,rooms,floor,rent,priceIndex
-X = [[45, 2, 3, 2600, 64286]]
-print X[0]
-X = scaler.transform(X)
-y_pred = clf.predict(X)
-print y_pred[0]
+    # load SVM fit
+    clf = utils.getSVM()
+    # load scaler
+    scaler = utils.getScaler()
+
+    # predict
+    X = [[livingArea, rooms, floor, rent, area.priceIndex]]
+    X = scaler.transform(X)
+    y_pred = int(clf.predict(X)[0])
+
+    # print prediction
+    print "Input: %s rooms, %s m^2, floor %s, rent %s in %s" % (
+        rooms, livingArea, floor, rent, areaName)
+    print "Prediction: %s" % y_pred
+
+    sys.exit(0)
+
+if __name__ == '__main__':
+    main()
